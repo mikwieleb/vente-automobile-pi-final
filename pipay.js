bf1361bca834d7b34b1d32bf83fd7c4ef33e9de7
mikwieleb <mik.wieleb@gmail.com>
2025-04-19 14:15:27 +0200

    Update pipay.js

--- pipay.js
+++ pipay.js
@@ -1,29 +1,22 @@
-// Initialisation du SDK Pi en mode testnet
-Pi.init({
-  version: "2.0",
-  sandbox: true, // très important pour testnet
-});
+Pi.init({ version: "2.0", sandbox: true });
 
-function payer() {
-  Pi.createPayment(
-    {
-      amount: 0.001,
-      memo: "Paiement test VenteAuto",
-      metadata: { action: "paiement_test" }
-    },
-    {
-      onReadyForServerApproval: function (paymentId) {
-        console.log("Paiement prêt à être approuvé côté serveur :", paymentId);
-      },
-      onReadyForServerCompletion: function (paymentId, txid) {
-        console.log("Paiement prêt pour finalisation :", paymentId, txid);
-      },
-      onCancel: function (paymentId) {
-        console.log("Paiement annulé :", paymentId);
-      },
-      onError: function (error, paymentId) {
-        console.error("Erreur pendant le paiement :", error, paymentId);
-      }
-    }
-  );
-}
+const paymentData = {
+  amount: 0.001,
+  memo: "Paiement Testnet",
+  metadata: { custom: "data" }
+};
+
+const payBtn = document.getElementById("payButton");
+
+payBtn.addEventListener("click", () => {
+  Pi.createPayment(paymentData)
+    .then(payment => {
+      return payment.complete();
+    })
+    .then(() => {
+      alert("Paiement réussi !");
+    })
+    .catch(error => {
+      console.error("Erreur paiement Pi:", error);
+    });
+});
